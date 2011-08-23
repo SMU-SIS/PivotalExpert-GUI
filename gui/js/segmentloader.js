f9590ff65822e9576454ce826b3b7fdea2afb7ff
Robert Chai <sg.robert@gmail.com>
2011-08-23 17:11:47 +0800

    Inserted general layout codes into pages related in paper prototype and encoded route-linkages based on ng:include
    Formatted foundations <div> classes into generally most of the current
    implemented HTML pages, with accordance to the paper_prototype.v1.4.
    Re-routed previous link implementation, to utilize Angular's ng:include
    method. Generally, front interface would look the same, but HTML codes
    have been mainly re-inserted.

--- gui/js/segmentloader.js
+++ gui/js/segmentloader.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
-	$("#right_panel").load("user_info.html");
+	//$("#right_panel").load("user_info.html");
 	
-	$("#user_profile_edit_link").click(function() {
-		$("#right_panel").load("user_edit.html");
-	});
+	//$("#user_profile_edit_link").click(function() {
+	//	$("#right_panel").load("user_edit.html");
+	//});
 });
--- gui/js/urlmapping.js
+++ gui/js/urlmapping.js
@@ -1,41 +1,64 @@
 function PageRouter_Master($route) { //router for the website itself
 	var self = this;
 	
-	//user management module
+	/* DEFAULT Re-Directs */
 	$route.otherwise({redirectTo: '/'});
 	
 	//should check based on whether user is logged on or not
-	$route.when('/',{template:'user_home.html'});
+	$route.when('/',{template:'dashboard.html'});
 	
-	//header re-directs
+	
+	/* HEADER Re-Directs */
+	$route.when('/welcome',{template:'welcome.html'});
+	$route.when('/dashboard',{template:'dashboard.html'});
+	
 	//institutions module
 	$route.when('/institutions',{template:'institution_index.html'});
+	$route.when('/institutions/add',{template:'institution_add.html'});
 	
 	//projects module
 	$route.when('/projects',{template:'project_index.html'});
 	$route.when('/projects/create',{template:'project_create.html'});
-	$route.when('/projects/viewtest',{template:'project_view.html'}); //temporary for testing.
+	
+	
+	$route.when('/projects/<project_id>',{template:'project_view.html'}); //TESTING
+	$route.when('/projects/<project_id>/view/owner',{template:''}); //TESTING
+	$route.when('/projects/<project_id>/view/dev',{template:''}); //TESTING
+	$route.when('/projects/<project_id>/bid',{template:''}); //TESTING
+	$route.when('/projects/<project_id>/bid/manage',{template:''}); //TESTING
+	$route.when('/projects/<project_id>/payment',{template:''}); //TESTING	
+	
 	
 	//developers module
 	$route.when('/developers',{template:'developer_index.html'});
+	$route.when('/developers/world/',{template:'worldmap.html'}); 
 	
-	//footer re-directs
-	//sitemap page
-	$route.when('/sitemap',{template:'sitemap.html'});
 	
-	//faq page
-	$route.when('/faq',{template:'faq.html'});
+	$route.when('/developers/<country_id>',{template:''}); //TESTING
 	
-	//termsofservice page
-	$route.when('/termsofservice',{template:'terms.html'});
 	
-	//contactus page
-	$route.when('/contactus',{template:'contactus.html'});
+	
+	/* FOOTER Re-Directs */
+	$route.when('/sitemap',{template:'sitemap.html'});					// sitemap page
+	$route.when('/faq',{template:'faq.html'});									// faq page
+	$route.when('/terms_of_service',{template:'terms.html'});				// terms of service page
+	$route.when('/contact_us',{template:'contactus.html'});				// contact us page
+	
+	
+	/* SUB-PAGES Re-Directs */
+	$route.when('/edit_profile',{template:'user_edit.html'});
+	$route.when('/badges/suggest',{template:'suggest_badge.html'});
+	$route.when('/inbox',{template:'inbox.html'});
+	
+	
+	
+	$route.when('/user/<user_id>/view_profile',{template:''});	// TESTING
+	$route.when('/badges/<user_id>',{template:''});	// TESTING
+	
 	
 	$route.onChange(function() {
 		self.params = $route.current.params;
 	});
-	 
 	$route.parent(this);
 }
 
