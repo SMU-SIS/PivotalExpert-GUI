ef2bbeccbbd665842fbbf7e139d5971fd15c5a1d
Daniel Tsou <jedimdan@gmail.com>
2011-09-13 22:42:55 +0800

    CLEANUP: Removed Angular tutorial files

--- gui/app/js/services.js
+++
@@ -1,7 +0,0 @@
-/* http://docs.angularjs.org/#!angular.service */
-
-angular.service('Phone', function($resource){
-  return $resource('../../jsonapi/phones/:phoneId.json', {}, {
-    query: {method:'GET', params:{phoneId:'phones'}, isArray:true}
-  });
-});
