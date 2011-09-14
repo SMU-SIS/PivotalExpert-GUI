3fc870a5bf698b6eb339db7d379ee276af963061
Robert Chai <sg.robert@gmail.com>
2011-09-14 20:47:41 +0800

    Made additional sub-tab changes for Project Management tab, under dashboard.html as requested by new guidelines.

--- gui/js/scripts.js
+++ gui/js/scripts.js
@@ -4,14 +4,25 @@ $(document).ready(function() {
 });
 
 
-function tabSwitch(new_tab, new_content) {  
+function tabSwitch_dashboard(new_tab, new_content) {  
 	document.getElementById('newsfeed_tab').className = '';  
-	document.getElementById('projects_tab').className = '';  
+	document.getElementById('projectsmgt_tab').className = '';  
 	document.getElementById('returnontime_tab').className = '';  
 	document.getElementById(new_tab).className = 'active';   
 	
 	document.getElementById('newsfeed_content').style.display = 'none';  
-	document.getElementById('projects_content').style.display = 'none';  
+	document.getElementById('projectsmgt_content').style.display = 'none';  
 	document.getElementById('returnontime_content').style.display = 'none';  
+	document.getElementById(new_content).style.display = 'block';     
+}  
+function tabSwitch_projectmgt(new_tab, new_content) {  
+	document.getElementById('workroom_tab').className = '';  
+	document.getElementById('projects_tab').className = '';  
+	document.getElementById('bids_tab').className = '';  
+	document.getElementById(new_tab).className = 'active';   
+	
+	document.getElementById('workroom_content').style.display = 'none';  
+	document.getElementById('projects_content').style.display = 'none';  
+	document.getElementById('bids_content').style.display = 'none';  
 	document.getElementById(new_content).style.display = 'block';     
 }  
