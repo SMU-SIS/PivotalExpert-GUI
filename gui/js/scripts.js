b14212717935e1fb4046f54291578960d737055f
Robert Chai <sg.robert@gmail.com>
2011-09-23 00:12:07 +0800

    Show only completed links on current PivotalExpert website.

--- gui/js/scripts.js
+++ gui/js/scripts.js
@@ -10,12 +10,12 @@ $(document).ready(function(){
 function tabSwitch_dashboard(new_tab, new_content) {  
 	document.getElementById('newsfeed_tab').className = '';  
 	document.getElementById('projectsmgt_tab').className = '';  
-	document.getElementById('returnontime_tab').className = '';  
+	//document.getElementById('returnontime_tab').className = '';  
 	document.getElementById(new_tab).className = 'active';   
 	
 	document.getElementById('newsfeed_content').style.display = 'none';  
 	document.getElementById('projectsmgt_content').style.display = 'none';  
-	document.getElementById('returnontime_content').style.display = 'none';  
+	//document.getElementById('returnontime_content').style.display = 'none';  
 	document.getElementById(new_content).style.display = 'block';     
 }  
 function tabSwitch_projectmgt(new_tab, new_content) {  
